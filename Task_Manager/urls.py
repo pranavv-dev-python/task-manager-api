8b855ba17bd3757e7903ccdfb750091806fbbbc9
pranavv-dev-python <pranavvijay2000@gmail.com>
2026-03-24 22:25:43 +0530

    added root route

--- Task_Manager/urls.py
+++ Task_Manager/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path , include
 
+def home(request):
+    return JsonResponse({"message": "API is working"})
+
+
+
 urlpatterns = [
-    
+    path('', home),
+
     path('admin/', admin.site.urls),
-    path('api/v1/' , include("accounts.urls")) ,
-    path('api/v1/' , include("Task.urls")) ,
-    path('api/v1/' , include("Analytics.urls")) ,
-    
+
+    path('api/v1/accounts/', include("accounts.urls")),
+    path('api/v1/tasks/', include("Task.urls")),
+    path('api/v1/analytics/', include("Analytics.urls")),
 ]
